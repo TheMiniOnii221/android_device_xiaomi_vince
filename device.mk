680751a04721e1b1d2b4a5b40c1d331d7b2e245b
GhostMaster69-dev <rathore6375@gmail.com>
2021-12-21 12:33:24 +0530

    vince: Import Lineage LiveDisplay 2.0 HAL
    * from lineage mido and rebranded to vince
    * Added lineage livedisplay hal sepolicy
    
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- device.mk
+++ device.mk
@@ -339,6 +339,10 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0.vendor \
     lights.msm8953
 
+# Live Display
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service.xiaomi_vince
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
