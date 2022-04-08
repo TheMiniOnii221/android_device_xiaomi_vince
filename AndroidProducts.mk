41df73feab386052a715098fa217213de05ed438
AnkitSavani111 <ankitmsavani12345@gmail.com>
2022-04-08 20:01:42 +0530

    vince: Adapt tree to Evolution-X

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/spark_vince.mk
+    $(LOCAL_DIR)/evolution_vince.mk
 
 COMMON_LUNCH_CHOICES := \
-    spark_vince-user \
-    spark_vince-userdebug \
-    spark_vince-eng
+    evolution_vince-user \
+    evolution_vince-userdebug \
+    evolution_vince-eng
