b21b89f3cb3253ee3dacca42886df1ed2c4b4b99
Ankit Savani <ankitmsavani12345@gmail.com>
2022-09-06 18:22:53 +0000

    vince: Update fp to raven september release
    Signed-off-by: Ankit Savani <ankitmsavani12345@gmail.com>

--- evolution_vince.mk
+++ evolution_vince.mk
@@ -48,4 +48,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vince-user 8.1.0 OPM1.171019.019 V11.0.2.0.OEGMIXM release-keys"
     
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/raven/raven:12/SQ3A.220705.004/8836240:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:13/TP1A.220905.004/8927612:user/release-keys"
