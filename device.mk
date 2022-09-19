f8666e20e3bbd0cc3b3f7d4b35f5498e00bf86b2
anandhan07 <anandhasayanan007jb@gmail.com>
2022-09-19 16:53:58 +0530

    Revert "vince: Don't enforce to build RRO overlay"
    This reverts commit 8b098d52ab6a6d1daebc82ca2fcd22523643ea0b.

--- device.mk
+++ device.mk
@@ -23,6 +23,7 @@ PRODUCT_SHIPPING_API_LEVEL := 25
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # RRO (Runtime Resource Overlay)
+PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
 
