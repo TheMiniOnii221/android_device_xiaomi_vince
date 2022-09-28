61cb422350ddf68f5d641ad618de0affe95f250f
Ankit Savani <ankitmsavani12345@gmail.com>
2022-09-28 16:43:22 +0000

    Revert "vince: props: Update blur props"
    This reverts commit ac6538dc680e04f02311c1d66bc31ccac9370165.

--- prop.mk
+++ prop.mk
@@ -122,8 +122,10 @@ bluetooth.hardware.power.operating_voltage_mv=3300
 
 # Blur
 PRODUCT_PRODUCT_PROPERTIES += \
-ro.launcher.blur.appLaunch=0 \
-ro.surface_flinger.supports_background_blur=1
+ro.surface_flinger.supports_background_blur=0 \
+ro.sf.blurs_are_expensive=0 \
+persist.sys.sf.disable_blurs=1 \
+ro.launcher.blur.appLaunch=0
 
 # Boot
 PRODUCT_VENDOR_PROPERTIES += \
